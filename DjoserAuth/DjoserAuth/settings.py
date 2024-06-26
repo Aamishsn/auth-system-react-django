ceda9b1bde1c9559e073113a7c3818dfeacf79c7
Aamishsn <aamishsn@gmail.com>
2024-06-26 19:31:42 +0500

    SETTINGS UPDATED

--- DjoserAuth/DjoserAuth/settings.py
+++ DjoserAuth/DjoserAuth/settings.py
@@ -45,6 +45,14 @@ INSTALLED_APPS = [
 
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -57,7 +65,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'DjoserAuth.urls'
-
 
 
 TEMPLATES = [
@@ -130,3 +137,55 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "malik.amman.99b@gmail.com"
+EMAIL_HOST_PASSWORD = "tmuchrktykacsqdu"
+
+
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "UPDATE_LAST_LOGIN": True,
+}
+
+# Djoser Settings
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'USER_CREATE_PASSWORD_RETYPE':True,
+    'ACTIVATION_URL':'/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL':True,
+    'SEND_CONFIRMATION_EMAIL':True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION':True,
+    'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}',
+    'SET_PASSWORD_RETYPE': True,
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    'TOKEN_MODEL': None,       # To Delete User Must Set it to None
+    'SERIALIZERS':{
+        # 'user_create': 'account.serializers.UserCreateSerializer',
+        # 'user': 'account.serializers.UserCreateSerializer',
+        # 'user_delete': 'djoser.serializers.UserDeleteSerializer',
+    },
+    # 'EMAIL': {
+    #     'activation': 'account.email.ActivationEmail',
+    #     'confirmation': 'account.email.ConfirmationEmail',
+    #     'password_reset': 'account.email.PasswordResetEmail',
+    #     'password_changed_confirmation': 'account.email.PasswordChangedConfirmationEmail',
+    # },
+
+
+}
